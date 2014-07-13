7349611e7b7b0c92870ea14d0f01d966aaab288d
Cory Redmond <acecheese@live.co.uk>
2014-07-13 21:19:09 +0100

    Added a way to store the votes if nobody is on the server. Although UNTESTED.

---
+++ BVotifier-Bungee/src/main/java/eu/ac3_servers/dev/bvotifier/bungee/configuration/VoteStorage.java
@@ -0,0 +1,142 @@
+package eu.ac3_servers.dev.bvotifier.bungee.configuration;
+
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.List;
+
+import com.google.common.io.ByteStreams;
+
+import eu.ac3_servers.dev.bvotifier.bungee.BVotifier;
+import eu.ac3_servers.dev.bvotifier.bungee.model.Vote;
+import net.md_5.bungee.api.config.ServerInfo;
+import net.md_5.bungee.api.plugin.Plugin;
+import net.md_5.bungee.config.Configuration;
+import net.md_5.bungee.config.ConfigurationProvider;
+import net.md_5.bungee.config.YamlConfiguration;
+
+public class VoteStorage {
+
+	private Plugin plugin;
+	private File file;
+	private Configuration config;
+
+	public VoteStorage(Plugin plugin) {
+		
+		this.plugin = plugin;
+		
+		this.file = new File(getDataFolder(), "votes.yml");
+		
+		if (!getDataFolder().exists()) {
+            getDataFolder().mkdir();
+        }
+        if (!file.exists()) {
+            try {
+            	file.createNewFile();
+                try (InputStream is = getResourceAsStream("votes.yml");
+                     OutputStream os = new FileOutputStream(file)) {
+                    ByteStreams.copy(is, os);
+                }
+            } catch (IOException e) {
+                throw new RuntimeException("Unable to create storage file", e);
+            }
+        }
+	}
+	
+	public Configuration getConfig(){
+		try {
+			return (this.config = ConfigurationProvider.getProvider(YamlConfiguration.class).load(this.file));
+		} catch (IOException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
+	public void saveConfig(){
+		try {
+			ConfigurationProvider.getProvider(YamlConfiguration.class).save(config, new File(getDataFolder(), "votes.yml"));
+		} catch (IOException e) {
+			e.printStackTrace();
+			plugin.getLogger().severe("Couldn't save storage file!");
+		}
+	}
+	
+	private File getDataFolder(){
+		return plugin.getDataFolder();
+	}
+	
+	private InputStream getResourceAsStream(String file){
+		return plugin.getResourceAsStream(file);
+	}
+	
+	public void storeVote(Vote vote, ServerInfo... destination){
+		
+		for (ServerInfo serverInfo : destination) {
+			getConfig().set("votes."+ serverInfo.getName() + "." + vote.getTimeStamp() + ".name", vote.getUsername());
+			getConfig().set("votes."+ serverInfo.getName() + "." + vote.getTimeStamp() + ".address", vote.getAddress());
+			getConfig().set("votes."+ serverInfo.getName() + "." + vote.getTimeStamp() + ".service", vote.getServiceName());
+		}
+		
+	}
+	
+	public void loadVotes(ServerInfo serverInfo){
+		
+		this.plugin.getProxy().getScheduler().runAsync(plugin, new LoadVoteTask(this.plugin, getConfig().getSection("votes"), serverInfo));
+		return;
+		
+	}
+}
+
+class LoadVoteTask implements Runnable {
+	
+	private Configuration section;
+	@SuppressWarnings("unused")
+	private Plugin plugin;
+	private ServerInfo server;
+
+	public LoadVoteTask(Plugin plugin, Configuration section, ServerInfo server) {
+		
+		this.plugin = plugin;
+		this.section = section;
+		this.server = server;
+		
+	}
+
+	@Override
+	public void run() {
+		String serverName = server.getName();
+		List<Long> index = section.getLongList(serverName);
+		for (Long time : index) {
+			Vote vote = new Vote(
+					section.getString(serverName + "." + time + ".service"),
+					section.getString(serverName + "." + time + ".name"),
+					section.getString(serverName + "." + time + ".address"),
+					""+time.longValue()
+			);
+			BVotifier.getInstance().d(vote.toString());
+			
+			ByteArrayOutputStream b = new ByteArrayOutputStream();
+			DataOutputStream out = new DataOutputStream(b);
+			
+			try {
+				out.writeUTF(vote.getAddress());
+				out.writeUTF(vote.getServiceName());
+				out.writeUTF(vote.getTimeStamp());
+				out.writeUTF(vote.getUsername());
+			} catch (IOException e1) {
+				e1.printStackTrace();
+			}
+			if(!server.getPlayers().isEmpty()){
+				
+				server.sendData(BVotifier.channelName, b.toByteArray());
+				
+			}
+		}
+		
+	}
+	
+}
--- BVotifier-Bungee/src/main/java/eu/ac3_servers/dev/bvotifier/bungee/relay/VoteRelay.java
+++ BVotifier-Bungee/src/main/java/eu/ac3_servers/dev/bvotifier/bungee/relay/VoteRelay.java
@@ -3,20 +3,32 @@ package eu.ac3_servers.dev.bvotifier.bungee.relay;
 import java.io.ByteArrayOutputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Map;
 
 import eu.ac3_servers.dev.bvotifier.bungee.*;
+import eu.ac3_servers.dev.bvotifier.bungee.configuration.VoteStorage;
 import eu.ac3_servers.dev.bvotifier.bungee.model.*;
 import net.md_5.bungee.api.config.ServerInfo;
+import net.md_5.bungee.api.event.ServerSwitchEvent;
 import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.event.EventHandler;
 
 public class VoteRelay implements Listener {
 	
 	private BVotifier plugin;
+	private VoteStorage voteStorage;
 
 	public VoteRelay(BVotifier plugin) {
 		this.plugin = plugin;
+		
+		this.voteStorage = new VoteStorage(plugin);
+	}
+	
+	@EventHandler
+	public void onServerChange(ServerSwitchEvent e){
+		ServerInfo serverInfo = e.getPlayer().getServer().getInfo();
+		voteStorage.loadVotes(serverInfo);
 	}
 	
 	@EventHandler
@@ -36,10 +48,20 @@ public class VoteRelay implements Listener {
 		}
 
 		Map<String, ServerInfo> servers = this.plugin.getProxy().getServers();
-		
-		for (String serverName : servers.keySet()) {
-			if(this.plugin.isDebug()) this.plugin.getLogger().info("Sending a vote to: " + serverName + "for " + vote.getUsername());
-			servers.get(serverName).sendData(BVotifier.channelName, b.toByteArray());
+		ArrayList<ServerInfo> destinations = new ArrayList<ServerInfo>();
+		for (ServerInfo server : servers.values()) {
+			if(server.getPlayers().isEmpty()){
+				destinations.add(server);
+				if(this.plugin.isDebug()) this.plugin.getLogger().info("Server: " + server.getName() + " has no players so queued");
+			}else{
+				if(this.plugin.isDebug()) this.plugin.getLogger().info("Sending a vote to: " + server.getName() + "for " + vote.getUsername());
+				server.sendData(BVotifier.channelName, b.toByteArray());
+			}
+		}
+		if(!destinations.isEmpty()){
+			
+			this.voteStorage.storeVote(vote, destinations.toArray(new ServerInfo[destinations.size()]));
+			
 		}
 	}
 
