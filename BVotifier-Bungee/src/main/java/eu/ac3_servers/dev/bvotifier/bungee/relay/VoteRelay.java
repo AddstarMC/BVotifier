9ac30ff54a97aa6a543b01f99093d93411d720c6
addstar <addstar@addstar.com.au>
2014-09-16 17:39:27 +1000

    Add option to relay vote only to the server that player is on (also has default server if player offline)

--- BVotifier-Bungee/src/main/java/eu/ac3_servers/dev/bvotifier/bungee/relay/VoteRelay.java
+++ BVotifier-Bungee/src/main/java/eu/ac3_servers/dev/bvotifier/bungee/relay/VoteRelay.java
@@ -5,11 +5,13 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Map;
+import java.util.logging.Level;
 
 import eu.ac3_servers.dev.bvotifier.bungee.*;
 import eu.ac3_servers.dev.bvotifier.bungee.configuration.VoteStorage;
 import eu.ac3_servers.dev.bvotifier.bungee.model.*;
 import net.md_5.bungee.api.config.ServerInfo;
+import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.event.ServerSwitchEvent;
 import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.event.EventHandler;
@@ -47,21 +49,46 @@ public class VoteRelay implements Listener {
 			e1.printStackTrace();
 		}
 
-		Map<String, ServerInfo> servers = this.plugin.getProxy().getServers();
-		ArrayList<ServerInfo> destinations = new ArrayList<ServerInfo>();
-		for (ServerInfo server : servers.values()) {
-			if(!this.plugin.emptysend && server.getPlayers().isEmpty()){
-				//destinations.add(server);
-				this.plugin.getLogger().info("Server: " + server.getName() + " has no players so dropped.");
+		if (this.plugin.singleServerVote){
+			// Only send to the server that the player is on (or default if not online)
+			ProxiedPlayer pp = plugin.getProxy().getPlayer(vote.getUsername());
+			ServerInfo server = null;
+			if (pp != null){
+				// send to player's server
+				server = pp.getServer().getInfo();
 			}else{
-				if(this.plugin.isDebug()) this.plugin.getLogger().info("Sending a vote to: " + server.getName() + "for " + vote.getUsername());
-				server.sendData(BVotifier.channelName, b.toByteArray());
+				// send to default server
+				server = plugin.getProxy().getServerInfo(this.plugin.defaultVoteServer);
 			}
-		}
-		if(!destinations.isEmpty()){
 			
-			this.voteStorage.storeVote(vote, destinations.toArray(new ServerInfo[destinations.size()]));
-			
+			if (server != null){
+				if(!this.plugin.emptysend && server.getPlayers().isEmpty()){
+					this.plugin.getLogger().info("Server: " + server.getName() + " has no players so dropped.");
+				}else{
+					if(this.plugin.isDebug()) this.plugin.getLogger().info("Sending a vote to: " + server.getName() + "for " + vote.getUsername());
+					server.sendData(BVotifier.channelName, b.toByteArray());
+				}
+			}else{
+				this.plugin.getLogger().log(Level.WARNING, "Player is offline and default vote server does not exist!");
+			}
+		}else{
+			// Send to all active servers
+			Map<String, ServerInfo> servers = this.plugin.getProxy().getServers();
+			ArrayList<ServerInfo> destinations = new ArrayList<ServerInfo>();
+			for (ServerInfo server : servers.values()) {
+				if(!this.plugin.emptysend && server.getPlayers().isEmpty()){
+					//destinations.add(server);
+					this.plugin.getLogger().info("Server: " + server.getName() + " has no players so dropped.");
+				}else{
+					if(this.plugin.isDebug()) this.plugin.getLogger().info("Sending a vote to: " + server.getName() + "for " + vote.getUsername());
+					server.sendData(BVotifier.channelName, b.toByteArray());
+				}
+			}
+			if(!destinations.isEmpty()){
+
+				this.voteStorage.storeVote(vote, destinations.toArray(new ServerInfo[destinations.size()]));
+
+			}
 		}
 	}
 
